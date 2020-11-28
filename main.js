4ad774590eea82b40e38be1b5cece40dfe1b8623
Your Name <bobbynicholson78704@gmail.com>
2020-11-28 10:27:26 -0600

    dynamic date

--- main.js
+++ main.js
@@ -1,43 +1,48 @@
-            //Nav Stuff
-            let mobileMenu = document.querySelector(".mobile-menu");
-            let navOpen = document.querySelector(".mobile-dropper");
-          
-            let navClose = document.querySelector(".m-x");
-            let anchors = document.querySelectorAll(".m-nav-anchor");
-            let hrs = document.querySelectorAll(".m-nav-list hr");
-            navOpen.addEventListener("click", () => {
-              anchors.forEach(anchor => {
-                anchor.style.display = "block";
-                // anchor.classList.add('jello')
-              });
-            
-              hrs.forEach(hr => {
-                hr.style.display = "block";
-              });
-            
-              mobileMenu.style.width = "300px";
-              mobileMenu.classList.add("bounceInRight");
-              navClose.style.color = "white";
-            });
-            
-            navClose.addEventListener("click", () => {
-              mobileMenu.style.width = "0";
-              mobileMenu.classList.remove("bounceInRight");
-              navClose.style.color = "rgba(255, 255, 255, 0)";
-              anchors.forEach(anchor => {
-                anchor.style.display = "none";
-                // anchor.classList.remove('jello')
-              });
-            
-              hrs.forEach(hr => {
-                hr.style.display = "none";
-              });
-            });
+//Nav Stuff
+let mobileMenu = document.querySelector(".mobile-menu");
+let navOpen = document.querySelector(".mobile-dropper");
+let footerDate = document.getElementById("footerDate");
+let navClose = document.querySelector(".m-x");
+let anchors = document.querySelectorAll(".m-nav-anchor");
+let hrs = document.querySelectorAll(".m-nav-list hr");
+navOpen.addEventListener("click", () => {
+  anchors.forEach((anchor) => {
+    anchor.style.display = "block";
+    // anchor.classList.add('jello')
+  });
 
-            
+  hrs.forEach((hr) => {
+    hr.style.display = "block";
+  });
 
-            // When the user scrolls the page, execute myFunction
-window.onscroll = function() {myFunction()};
+  mobileMenu.style.width = "300px";
+  mobileMenu.classList.add("bounceInRight");
+  navClose.style.color = "white";
+});
+
+navClose.addEventListener("click", () => {
+  mobileMenu.style.width = "0";
+  mobileMenu.classList.remove("bounceInRight");
+  navClose.style.color = "rgba(255, 255, 255, 0)";
+  anchors.forEach((anchor) => {
+    anchor.style.display = "none";
+    // anchor.classList.remove('jello')
+  });
+
+  hrs.forEach((hr) => {
+    hr.style.display = "none";
+  });
+});
+
+// DYNAMIC DATE FOR FOOTER
+let d = new Date();
+let n = d.getFullYear();
+footerDate.textContent = n;
+
+// When the user scrolls the page, execute myFunction
+window.onscroll = function () {
+  myFunction();
+};
 
 // Get the navbar
 let navbar = document.getElementById("navbar");
@@ -48,11 +53,8 @@ let sticky = navbar.offsetTop;
 // Add the sticky class to the navbar when you reach its scroll position. Remove "sticky" when you leave the scroll position
 function myFunction() {
   if (window.pageYOffset > sticky) {
-    navbar.classList.add("sticky")
-   
+    navbar.classList.add("sticky");
   } else {
     navbar.classList.remove("sticky");
   }
 }
-        
-
